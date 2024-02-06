c15b1a2a9bf8dd25e975eaf867d3dac00dd4cde8
a7medalyapany <sci.ahmedibrahim76837@alexu.edu.eg>
2024-02-06 02:28:36 +0200

    chore: delete recharts dependency

---
+++ components/shared/profile/AnswerTab.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+interface AnswerTabProps {}
+
+const AnswerTab: FC<AnswerTabProps> = () => {
+  return <div>AnswerTab</div>;
+};
+
+export default AnswerTab;
---
+++ components/shared/profile/QuestionTab.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+interface QuestionTabProps {}
+
+const QuestionTab: FC<QuestionTabProps> = () => {
+  return <div>QuestionTab</div>;
+};
+
+export default QuestionTab;
